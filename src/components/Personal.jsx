b4b204ebd54a615fe5caeadbc5cf467b5c43360c
jarryingnut <rabhishekranjan@gmail.com>
2022-10-26 18:49:41 +0530

    complete Personal Information component

--- src/components/Personal.jsx
+++ src/components/Personal.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import styled from 'styled-components'
+import Input from './Input'
+import TextArea from './TextArea'
+
+const Wrapper = styled.form`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 1.5rem;
+
+  .title, .textArea {
+    grid-column: 1/-1;
+  }
+`
 
 const Personal = () => {
   return (
-    <div>Personal</div>
+    <Wrapper>
+      <h2 className='section-heading'>Personal Information</h2>
+      <Input placeholder='First Name'/>
+      <Input placeholder='Last Name'/>
+      <Input placeholder='Title (e.g Backend Engineer)' className='title'/>
+      <Input placeholder='Email'/>
+      <Input placeholder='Phone'/>
+      <Input placeholder='LinkedIn username'/>
+      <Input placeholder='Github username'/>
+      <TextArea className='textArea' placeholder='describe yourself here' rows={3}/>
+    </Wrapper>
   )
 }
 

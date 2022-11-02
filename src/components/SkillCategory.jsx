82dd642f769c88052d709758efdd3e42774cccc2
jarryingnut <rabhishekranjan@gmail.com>
2022-11-02 17:47:41 +0530

    add dynamic skill input to skill category

--- src/components/SkillCategory.jsx
+++ src/components/SkillCategory.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Input from './common/Input'
 import styled from 'styled-components'
 import Button from './common/Button'
@@ -28,7 +28,34 @@ const Wrapper = styled.form`
 
 `
 
+
+const Skill = ({innerForm, setInnerForm, id}) => {
+
+  const removeInnerForm = (e) => {
+    e.preventDefault();
+    setInnerForm(innerForm.filter((_, index) => {
+      return id !== index
+    }))
+  }
+
+  return (
+    <div className='flex-container-sub'>
+      <Input className="skill-category" placeholder='skill'/>
+      <Button className='close-skill-category' onClick={removeInnerForm}>X</Button>
+    </div>
+  )
+}
+
+
 const SkillCategory = ({skillForm, setSkillForm, id}) => {
+
+  const [innerForm, setInnerForm] = useState([])
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    setInnerForm([...innerForm, Skill])
+  }
+
   const removeSkillCategory = (e) => {
     e.preventDefault();
     setSkillForm(skillForm.filter((_, index) => {
@@ -43,11 +70,11 @@ const SkillCategory = ({skillForm, setSkillForm, id}) => {
       <Input className="skill-category-skill" placeholder='skill category'/>
       <Button className='close-skill-category-skill' onClick={removeSkillCategory}>X</Button>
       </div>
-      <div className='flex-container-sub'>
-          <Input className="skill-category" placeholder='skill'/>
-          <Button className='close-skill-category'>X</Button>
-      </div>
-    <Button className='add-skill' >Add Skill</Button>
+
+      {
+          innerForm.map((Form, i) => <Form key={i} id={i} innerForm={innerForm} setInnerForm={setInnerForm}/>)
+      }
+    <Button className='add-skill' onClick={handleClick}>Add Skill</Button>
     </Wrapper>
   )
 }
